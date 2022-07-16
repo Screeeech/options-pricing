b3319a9705736f62c3cb8a9378e0eae7f053a8d0
Hasith <hasith200311@gmail.com>
2022-07-16 14:49:33 -0500

    added payoff function

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -11,17 +11,22 @@ def step_function(s, delt, mu, sigma):
     return s + mu * s * delt + sigma * s * random_function() * np.sqrt(delt)
 
 
-def create_monte_carlo(init_price, end, period, mu= .12, sigma=.7, n=256):
-    p_df = pd.DataFrame(columns=["time-" + period, "price"])
+def payoff(df, k):
+    return max(df["price"].iloc[-1] - k, 0)
+
+
+def create_monte_carlo(init_price, end, mu=.12, sigma=.7, n=256):
+    p_df = pd.DataFrame(columns=["time", "price"])
 
     p_df.loc[0] = [0, init_price]
 
     for i in range(1, end * n + 1):
-        p_df.loc[i] = [i/n, step_function(p_df["price"][i-1], end/n, mu, sigma)]
+        p_df.loc[i] = [i / n, step_function(p_df["price"][i - 1], end / n, mu, sigma)]
 
-    plt.plot(p_df["time-" + period], p_df["price"])
-    plt.show()
+    return p_df
 
 
 if __name__ == '__main__':
-    create_monte_carlo(40, 1, "days")
+    p_df = create_monte_carlo(40, 1)
+    plt.plot(p_df["time"], p_df["price"])
+    plt.show()
