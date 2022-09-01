8f438fe95f36f6a20c5727b37fc900421ddccb8a
Hasith <hasith200311@gmail.com>
2022-08-31 21:27:06 -0500

    changed code to reflect normal probability density

--- absolute_risk.py
+++ absolute_risk.py
@@ -5,14 +5,6 @@ from options_pricer import symbol_parts
 from scipy.stats import norm
 
 
-def caculate_risk(s, bets):
-    risk = []
-    for i in range(bets.shape[0]):
-        risk.append((symbol_parts(bets.iloc[i].contractSymbol)[3] - s + bets.iloc[i]["ask"]) / s)
-
-    bets["risk"] = risk
-
-
 def return_at_s(s, x, k):
     return np.maximum(s - x, 0) - k
 
@@ -22,85 +14,13 @@ def return_of_bets_at_s(bets, s):
                    range(bets.shape[0])])
 
 
-# Do not use, integration does not work with this function
-# Use lreimann()
-def returns_over_s(s, mu, sigma, bets):
-    return integrate.quad(lambda x: return_of_bets_at_s(bets, x), s * (1 + mu + sigma), s * (1 + mu - sigma))
-
-
-def lreimann(lbound, ubound, contract_symbol, delx):
+def lreimann(lbound, ubound, contract_symbol, k, probability_density, delx):
     x = np.linspace(lbound, ubound, int(delx * (ubound - lbound)))
-    k = symbol_parts(contract_symbol)[3]
-
-    return np.sum([return_at_s(contract_symbol, x[i], k) / delx for i in range(len(x) - 1)])
+    return np.sum([return_at_s(x[i], symbol_parts(contract_symbol)[3], k)*probability_density(x[i]) / delx for i in range(len(x) - 1)])
 
 
-def trap_reimann(lbound, ubound, bets, delx, mu, sigma):
-    x = np.linspace(lbound, ubound, int(delx * (ubound - lbound)))
-    base1 = return_of_bets_at_s(bets, x[0])
-    integral = 0
-    for i in range(len(x) - 1):
-        base2 = return_of_bets_at_s(bets, x[i + 1])
-        # what is this formatting, if you're wrapping at least make it neat
-        integral += .5 * (x[i + 1] - x[i]) * (
-                    base1 * norm.pdf(x[i], x[i] * (1 + mu), x[i] * sigma) + base2 * norm.pdf(x[i + 1],
-                                                                                             x[i + 1] * (1 + mu),
-                                                                                             x[i + 1] * sigma))
-        base1 = base2
-    return integral
+def calculate_expected_return(contract_symbol, k, normal_function, lbound, ubound, dollar_resolution=20):
+    return lreimann(lbound, ubound, contract_symbol, k, normal_function, dollar_resolution)
 
 
-def calculate_expected_return(bet, k, normal_function, s, mu, sigma):
-    return lreimann(s*(1+mu-sigma), s*(1+mu-sigma), bet, 20)
-
-
-def find_max_integral(bets, function):
-    beg = 0
-    end = bets.shape[0]
-
-    result = -1
-    while beg != end:
-        mid = (beg + end) // 2
-        if function(mid) > function(end):
-            end = mid
-
-        else:
-            beg = mid + 1
-        result = beg
-
-    return result
-
-
-def uniform_risk_cutoff(ticker, s, bets, sigma_range=(-1, 1), dollar_resolution=20):
-    # bets = price_options(ticker, "2022-08-26", .091)
-    caculate_risk(s, bets)
-    bets.sort_values("risk")
-
-    dollar_resolution = np.minimum(dollar_resolution, 100)
-    mu, sigma = hanal.get_stats(ticker, 1)
-    max_int = find_max_integral(bets, lambda m: lreimann(s * (1 + mu + (sigma_range[0] * sigma)),
-                                                         s * (1 + mu + (sigma_range[1] * sigma)),
-                                                         bets.iloc[:m], dollar_resolution))
-
-    return max_int
-
-
-def normal_risk_cutoff(ticker, s, bets, sigma_range=(-1, 1), dollar_resolution=20):
-    # bets = price_options(ticker, "2022-08-26", .091)
-    caculate_risk(s, bets)
-    bets.sort_values("risk")
-
-    dollar_resolution = np.minimum(dollar_resolution, 100)
-    mu, sigma = hanal.get_stats(ticker, 1)
-    max_int = find_max_integral(bets, lambda m: trap_reimann(s * (1 + mu + (sigma_range[0] * sigma)),
-                                                             s * (1 + mu + (sigma_range[1] * sigma)),
-                                                             bets.iloc[:m], dollar_resolution, mu, sigma))
-
-    return max_int
-
-
-"""
-priced_options = price_options("aapl", "2022-08-26", .091)
-cutoff = risk_cutoff("aapl", 171.52, priced_options)
-print(cutoff)
-"""
+# print(calculate_expected_return("AAPL220902C00070000", 91.50, lambda x: norm.pdf(x, 161.50, 7), 161.50-7, 161.50+7))
--- dow_testing.py
+++ dow_testing.py
@@ -1,11 +1,14 @@
-import pandas as pd
 from options_pricer import price_options
-from absolute_risk import normal_risk_cutoff, uniform_risk_cutoff
+from history_analysis import get_stats
+from absolute_risk import calculate_expected_return
 import yfinance as yf
+from datetime import date
+from dateutil.relativedelta import relativedelta
+from scipy.stats import norm
+import pandas as pd
 import pickle
 
-uniform_bets = []
-normal_bets = []
+bets = []
 dow = pd.read_csv("dow.csv")
 
 for ticker in dow["Symbol"]:
@@ -14,17 +17,25 @@ for ticker in dow["Symbol"]:
         # maybe we can also return the last price in price_options()
         # that way we don't have to download data again at the end of
         # loop to input s
-        priced_options = price_options(ticker, "2022-08-26", .091)
+        priced_options = price_options(ticker, "2022-09-02", .091)
     except:
         continue
 
-    uniform_bets.append(priced_options[:uniform_risk_cutoff(ticker, yf.download(ticker).iloc[-1]["Adj Close"],
-                                                            priced_options)])
-    normal_bets.append(priced_options[:normal_risk_cutoff(ticker, yf.download(ticker).iloc[-1]["Adj Close"],
-                                                          priced_options)])
+    mu, std = get_stats("aapl", 1, end=date.today() - relativedelta(days=7))
 
-with open('uniform_dow_bets_2022-08-26.pkl', 'wb') as f:
-    pickle.dump(uniform_bets, f)
+    expected_returns = []
+    drops = []
+    for i in range(priced_options.shape[0]):
+        s = yf.download(ticker, progress=False).iloc[-1]["Adj Close"]
+        expected_returns.append(calculate_expected_return(priced_options.iloc[i]["contractSymbol"],
+                                                          priced_options.iloc[i]["ask"],
+                                                          lambda x: norm.pdf(x, s, std),
+                                                          s * (1 + mu - std), s * (1 + mu + std)))
+        if expected_returns[i] <= 0:
+            drops.append(i)
 
-with open('normal_bets_2022-08-26.pkl', 'wb') as f:
-    pickle.dump(normal_bets, f)
+    priced_options["expected_returns"] = expected_returns
+    bets.append(priced_options.drop(priced_options.index[drops]))
+
+with open('dow_bets.pickle', 'wb') as f:
+    pickle.dump(bets, f)
